e3783f977f0f126d871114062baa8116d05fe761
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2022-10-07 15:10:07 +0000

    Added docstring

--- main.py
+++ main.py
@@ -11,19 +11,18 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
+    """Returns days of a month using leap years"""
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
   if is_leap(year) and month ==2:
       return 29
   return month_days[month - 1]
       
-    
-#🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
 
-
+days_
 
 
 
